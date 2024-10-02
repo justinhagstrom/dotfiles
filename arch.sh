34226aed8e7b1849b5dde1c070423044351f7112
Justin Hagstrom <justinhagstrom@gmail.com>
2024-10-01 22:21:15 -0400

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,14 +1,25 @@
+# Initial setup of pacman
 sudo pacman-key --init;
 sudo pacman-key --populate;
 sudo reflector --country "US" --protocol https,http --score 20 --sort rate --save /etc/pacman.d/mirrorlist;
 sudo pacman -Syu --noconfirm;
 
-# reboot
+### Reboot here ###
 
-sudo pacman -S --noconfirm fastfetch tmux git neovim btop python-pip htop iotop iftop unzip gcc npm make ripgrep fd ranger go fish;
+# Install packages
+sudo pacman -S --noconfirm --needed tmux fastfetch neovim ripgrep fd \
+    ranger btop htop iotop iftop fish \
+    git python-pip base-devel unzip gcc npm make go;
 
+# Copy dotfiles
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.tmux.conf;
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/fastfetch/config.jsonc -P .config/fastfetch/;
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/btop/btop.conf -P .config/btop/;
 
 git clone https://github.com/justinhagstrom/kickstart.nvim "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim;
+
+# Install yay
+git clone https://aur.archlinux.org/yay.git;
+cd yay;
+makepkg -si --noconfirm;
+cd;
