79eaf197a9287eb6c906947fc799ff24d12987ab
Justin Hagstrom <justinhagstrom@gmail.com>
2024-10-02 22:49:58 -0400

    Update debian.sh

--- install_scripts/debian.sh
+++ install_scripts/debian.sh
@@ -10,7 +10,7 @@ sudo apt install -y tmux git neovim btop python3-pip htop iotop iftop unzip gcc 
 # Copy dotfiles
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.tmux.conf --backups=1;
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/fastfetch/config.jsonc -P .config/fastfetch/ --backups=1;
-wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/btop/btop.conf -P .config/btop/ --backups=1;
+#wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/btop/btop.conf -P .config/btop/ --backups=1;
 
 git clone https://github.com/justinhagstrom/kickstart.nvim "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim;
 
