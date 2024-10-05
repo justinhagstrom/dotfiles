7980ac6bb0496f85cfef7f590d62924aca927d84
Justin Hagstrom <justinhagstrom@gmail.com>
2024-10-05 13:07:43 -0400

    Update fedora.sh

--- install_scripts/fedora.sh
+++ install_scripts/fedora.sh
@@ -8,13 +8,12 @@ sudo dnf update -y;
 sudo dnf install -y awscli \
     tmux btop fastfetch htop iotop iftop cmatrix fish \
     git git-lfs gcc g++ make npm \
-    python3-pip wget unzip \
-    neovim lua go ripgrep fd-find ranger zellij;
+    python3-pip wget unzip cargo \
+    neovim lua go ripgrep fd-find ranger;
 
 # Copy dotfiles
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.tmux.conf --backups=1;
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/fastfetch/config.jsonc -P .config/fastfetch/ --backups=1;
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/btop/btop.conf -P .config/btop/ --backups=1;
-wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/zellij/layouts/default.kdl -P .config/zellij/layouts/ --backups=1;
 
 git clone https://github.com/justinhagstrom/kickstart.nvim "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim;
