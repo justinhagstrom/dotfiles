863b6d8d1a3d93b8614c4ee6905d4a63ea3f2e1c
Justin Hagstrom <justinhagstrom@gmail.com>
2025-08-18 14:54:54 -0400

    Update debian.sh

--- install_scripts/debian.sh
+++ install_scripts/debian.sh
@@ -6,11 +6,13 @@ sudo apt upgrade -y;
 # sudo reboot now;
 
 # Install packages
-sudo apt install -y tmux git neovim btop python3-pip htop iotop iftop unzip gcc npm fastfetch make ripgrep xclip curl golang;
+sudo apt install -y tmux git neovim btop python3-pip htop iotop iftop \
+    unzip gcc npm fastfetch make ripgrep xclip curl golang cargo mc fish;
 
 # Copy dotfiles
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.tmux.conf --backups=1;
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/fastfetch/config.jsonc -P .config/fastfetch/ --backups=1;
 wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/btop/btop.conf -P .config/btop/ --backups=1;
+wget https://raw.githubusercontent.com/justinhagstrom/dotfiles/refs/heads/main/.config/kitty/kitty.conf -P .config/kitty/ --backups=1;
 
-git clone https://github.com/justinhagstrom/kickstart.nvim "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim;
+# git clone https://github.com/justinhagstrom/kickstart.nvim "${XDG_CONFIG_HOME:-$HOME/.config}"/nvim;
